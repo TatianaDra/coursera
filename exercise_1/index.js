fed99bdc1291862a6b35769e99cd5f17acd0b1b4
Tatiana <tatiana.dracheva@gmail.com>
2018-11-24 23:21:41 +0300

    aff first exercise of coursera

---
+++ exercise_1/index.js
@@ -0,0 +1,10 @@
+/**
+ * @param {Number} a Первое слагаемое
+ * @param {Number} b Второе слагаемое
+ * @returns {Number}
+ */
+module.exports = function (a, b) {
+	a=parseInt(a,10);
+	b=parseInt(b,10);
+return a+b;
+};
