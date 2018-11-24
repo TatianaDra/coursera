20a6217f781d7b4b37aa30457811031a99f6a404
Tatiana <tatiana.dracheva@gmail.com>
2018-11-25 02:03:57 +0300

    add function for adding interval to time

---
+++ exercise_3_summTime/index.js
@@ -0,0 +1,27 @@
+/**
+ * @param {Number} hours
+ * @param {Number} minutes
+ * @param {Number} interval
+ * @returns {String}
+ */
+module.exports = function (hours, minutes, interval) {
+    minutes = minutes + interval;
+    while (minutes > 59) {
+        hours + = 1;
+        minutes = minutes - 60;
+    }
+    while (hours > 23) {
+        hours = hours - 24;
+    }
+    if (hours < 10)
+        hours = "0" + hours;
+    if (minutes < 10)
+        minutes = "0" + minutes;
+
+
+    return
+    hours + ":" + minutes;
+
+
+}
+;
